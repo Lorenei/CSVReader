23188699671b4ec4fbf96929c62680c400c6c07c
Arkadiusz Richert <arshenik@gmail.com>
2017-12-10 12:14:06 +0100

    Initial commit

---
+++ CSVReaderConsole/CSVReaderConsole/CSVReaderClass.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSVReaderConsole {
+    class CSVReaderClass {
+        string[] Arguments;
+        public CSVReaderClass(string[] args) {
+            Arguments = args;
+            if (checkIfArgumentsAreEntered()) Console.WriteLine("Są argumenty");
+            else Console.WriteLine("Brak argumentów");
+        }
+
+        private bool checkIfArgumentsAreEntered() {
+            return (Arguments.Length > 0) ? true : false;
+        }
+    }
+}
