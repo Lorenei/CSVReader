c8019879ce2b27f8ecd1bf26b4d998e1ea3086dc
Arkadiusz Richert <arshenik@gmail.com>
2017-12-10 13:04:03 +0100

    initial commit

--- CSVReaderConsole/CSVReaderConsole/CSVReaderClass.cs
+++ CSVReaderConsole/CSVReaderConsole/CSVReaderClass.cs
@@ -5,16 +5,20 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace CSVReaderConsole {
-    class CSVReaderClass {
-        string[] Arguments;
-        public CSVReaderClass(string[] args) {
-            Arguments = args;
-            if (checkIfArgumentsAreEntered()) Console.WriteLine("Są argumenty");
-            else Console.WriteLine("Brak argumentów");
-        }
+	class CSVReaderClass {
+		string [] Arguments;
+		public CSVReaderClass( string [] args ) {
+			Arguments = args;
+			if ( checkIfArgumentsAreEntered() ) Console.WriteLine( "Są argumenty" );
+			else Console.WriteLine( "Brak argumentów" );
+		}
 
-        private bool checkIfArgumentsAreEntered() {
-            return (Arguments.Length > 0) ? true : false;
-        }
-    }
+		private bool checkIfArgumentsAreEntered() {
+			return ( Arguments.Length > 0 ) ? true : false;
+		}
+		private void appExit() {
+			Console.WriteLine( "Application shutdown." );
+			
+		}
+	}
 }
